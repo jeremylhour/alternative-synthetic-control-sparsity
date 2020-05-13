19977706383173e45d32c1f063b99e0e7b0458e8
Jeremy L&#39;Hour <jeremy.lhour@insee.fr>
2020-05-13 11:00:17 +0200

    cleaning up Lalonde example

--- examples/EX2_Lalonde1986.R
+++ examples/EX2_Lalonde1986.R
@@ -18,12 +18,12 @@ set.seed(9081993)
 library("plyr")
 library("ggplot2")
 library("lbfgs")
+library("causalsens")
 
 ### Load user-defined functions
 source("functions/CalibrationLasso.R")
 source("functions/OrthogonalityReg.R")
-source("functions/ImmunizedATT.R")
-source("functions/Naive_ATT.R")
+source("functions/Compute_ATT.R")
 source("functions/LogitLasso.R")
 source("functions/BCHDoubleSelec.R")
 source("functions/LassoFISTA.R")
@@ -36,24 +36,20 @@ mMscale <- function(X){
   return(scale(X, center=mins, scale=maxs-mins))
 }
 
-# Load data
-{ 
-  library("causalsens")
+# LOAD AND MANAGE DATA
+library("causalsens")
 data(lalonde.psid)
 
 d <- lalonde.psid[,"treat"]
 y <- lalonde.psid[,"re78"]
 
 ### Transformations of the original dataset to have high-dimension data
-### Rescale continuous variables ?
 X.Main <- data.frame("Cons"=1,
                      lalonde.psid[,c("age","education","married","black","hispanic","re74","re75","nodegree")],
                      "NoIncome74"=as.numeric(lalonde.psid[,"re74"]==0),
                      "NoIncome75"=as.numeric(lalonde.psid[,"re75"]==0)
 )
 
-
-# X.Main[,c("age","education","re74","re75")] <- scale(X.Main[,c("age","education","re74","re75")])
 X.Main[,c("age","education","re74","re75")] <- mMscale(X.Main[,c("age","education","re74","re75")])
 
 X.ContInterac <- data.frame(
@@ -64,11 +60,11 @@ X.ContInterac <- data.frame(
   "AgexNoIncome74"=lalonde.psid[,"age"]*(lalonde.psid[,"re74"]==0),
   "AgexNoIncome75"=lalonde.psid[,"age"]*(lalonde.psid[,"re75"]==0),
   "EducxMarried"=lalonde.psid[,"education"]*lalonde.psid[,"married"],
-  "EdcuxNodegree"=lalonde.psid[,"education"]*lalonde.psid[,"nodegree"],
-  "EdcuxBlack"=lalonde.psid[,"education"]*lalonde.psid[,"black"],
-  "EdcuxHispanic"=lalonde.psid[,"education"]*lalonde.psid[,"hispanic"],
-  "EdcuxNoIncome74"=lalonde.psid[,"education"]*(lalonde.psid[,"re74"]==0),
-  "EdcuxNoIncome75"=lalonde.psid[,"education"]*(lalonde.psid[,"re75"]==0),
+  "EducxNodegree"=lalonde.psid[,"education"]*lalonde.psid[,"nodegree"],
+  "EducxBlack"=lalonde.psid[,"education"]*lalonde.psid[,"black"],
+  "EducxHispanic"=lalonde.psid[,"education"]*lalonde.psid[,"hispanic"],
+  "EducxNoIncome74"=lalonde.psid[,"education"]*(lalonde.psid[,"re74"]==0),
+  "EducxNoIncome75"=lalonde.psid[,"education"]*(lalonde.psid[,"re75"]==0),
   "Income74xMarried"=lalonde.psid[,"re74"]*lalonde.psid[,"married"],
   "Income74xNoDegree"=lalonde.psid[,"re74"]*lalonde.psid[,"nodegree"],
   "Income74xBlack"=lalonde.psid[,"re74"]*lalonde.psid[,"black"],
@@ -81,7 +77,6 @@ X.ContInterac <- data.frame(
   "Income75xNoIncome74"=lalonde.psid[,"re75"]*(lalonde.psid[,"re74"]==0)
 )
 
-# X.ContInterac <- scale(X.ContInterac)
 X.ContInterac <- mMscale(X.ContInterac)
 
 X.DumInterac <- data.frame(
@@ -102,69 +97,15 @@ X.DumInterac <- data.frame(
 )
 
 X.Poly <- poly(as.matrix(lalonde.psid[,c("age","education","re74","re75")]),degree=5)
-# X.Poly <- scale(X.Poly)
 X.Poly <- mMscale(X.Poly)
-# X.Educ <- model.matrix(~as.factor(lalonde.psid[,"education"] - 1))[,-1]
 X.Age <- model.matrix(~as.factor(lalonde.psid[,"age"]) - 1)[,-1]
 
 X <- cbind(X.Main,X.ContInterac,X.DumInterac,X.Poly)
 X <- as.matrix(X)
-}
 
 
-### 2. Calibration part
+### 2. Balancing
 CAL <- CalibrationLasso(d,X,c=1.1,maxIterPen=1e4,PostLasso=T,trace=T)
-
-# Comments: 19 iterations needed, select 8 variables
-
-# Checking covariate balancing
-# Lasso:
-W_Lasso <- (1-d) * exp(X%*%CAL$betaLasso)/sum(d)
-
-# Post-Lasso : 
-W_PL <- (1-d) * exp(X%*%CAL$betaPL)/sum(d)
-
-DVar <- 100*(W_PL - W_Lasso)/W_Lasso
-
-plot(W_Lasso,DVar)
-abline(h=0,lty=6)
-
-# data <- list(X=X,y=y,d=d,W=W_Lasso)
-# save(data,file="Lalondedata_Unscaled.R")
-
-BalanceCheck <- data.frame(Method=character(),
-                           VarID=double(),
-                           mDiff=double(),
-                           stringsAsFactors=FALSE)
-p <- ncol(X)
-
-BalanceCheck[1:p,"Method"] <- rep("Original",p)
-BalanceCheck[1:p,"VarID"] <- names(cbind(X.Main,X.ContInterac,X.DumInterac,X.Poly))
-BalanceCheck[1:p,"mDiff"] <- c(t(d/sum(d) - (1-d)/sum(1-d))%*%X)
-
-BalanceCheck[(p+1):(2*p),"Method"] <- rep("Lasso",p)
-BalanceCheck[(p+1):(2*p),"VarID"] <- names(cbind(X.Main,X.ContInterac,X.DumInterac,X.Poly))
-BalanceCheck[(p+1):(2*p),"mDiff"] <- c(t(d/sum(d) - W_Lasso)%*%X)
-
-BalanceCheck[(2*p+1):(3*p),"Method"] <- rep("PostLasso",p)
-BalanceCheck[(2*p+1):(3*p),"VarID"] <- names(cbind(X.Main,X.ContInterac,X.DumInterac,X.Poly))
-BalanceCheck[(2*p+1):(3*p),"mDiff"] <- c(t(d/sum(d) - W_PL)%*%X)
-
-BCheck <- subset(BalanceCheck,VarID%in%c("age","education","married","black","hispanic"))
-M <- max(abs(BCheck$mDiff))
-lb <- 1.1*M
-ub <- -1.1*M
-
-pdf("plots/LalondeBalanceCheck.pdf", width=12, height=12)
-ggplot(BCheck, aes(x = mDiff, y = factor(Method, level=c("PostLasso","Lasso","Original")))) + 
-  geom_point(aes(colour=factor(Method)), size=4) +
-  facet_wrap(~VarID, ncol=1) +
-  theme(plot.title = element_text(lineheight=.8, face="bold"),legend.position="none") +
-  scale_x_continuous(limits=c(ub,lb),name="Difference in mean, Treated-Control") + 
-  ylab("Balancing Method") +
-  ggtitle("Balancing check") 
-dev.off()
-
 
 ### 3. Computes the orthogonality parameter
 ORT_WLS <- OrthogonalityReg(y,d,X,CAL$betaLasso,method="WLSLasso",
@@ -186,30 +127,30 @@ Results <- data.frame(Estimator=character(),
                       Outcome=integer(),
                       stringsAsFactors=FALSE)
 ### 1. Naive Lasso
-Results[1,"Estimator"] <- c("Naive Plug-In, Lasso")
-Results[1,"ATT"] <- Naive_ATT(y,d,X,CAL$betaLasso)$theta
-Results[1,"asymptoticsd"] <-  Naive_ATT(y,d,X,CAL$betaLasso)$sigma
+Results[1,"Estimator"] <- c("Naive Plug-In -- Lasso")
+Results[1,"ATT"] <- Compute_ATT(y,d,X,CAL$betaLasso)$theta
+Results[1,"asymptoticsd"] <-  Compute_ATT(y,d,X,CAL$betaLasso)$sigma
 Results[1,"PropScore"] <- length(CAL$SHat)
 Results[1,"Outcome"] <- 0
 
 ### 2. Immunized Lasso
-Results[2,"Estimator"] <- c("Immunized, Lasso")
-Results[2,"ATT"] <- ImmunizedATT(y,d,X,CAL$betaLasso,ORT_WLS$muLasso, Immunity=T)$theta
-Results[2,"asymptoticsd"] <- ImmunizedATT(y,d,X,CAL$betaLasso,ORT_WLS$muLasso, Immunity=T)$sigma
+Results[2,"Estimator"] <- c("Immunized -- Lasso")
+Results[2,"ATT"] <- Compute_ATT(y,d,X,CAL$betaLasso,ORT_WLS$muLasso)$theta
+Results[2,"asymptoticsd"] <- Compute_ATT(y,d,X,CAL$betaLasso,ORT_WLS$muLasso)$sigma
 Results[2,"PropScore"] <- length(CAL$SHat)
 Results[2,"Outcome"] <- length(ORT_WLS$SHat)
 
 ### 3. Naive Post-Lasso
-Results[3,"Estimator"] <- c("Naive Plug-In, Post-Lasso")
-Results[3,"ATT"] <- Naive_ATT(y,d,X,CAL$betaPL)$theta
-Results[3,"asymptoticsd"] <- Naive_ATT(y,d,X,CAL$betaPL)$sigma
+Results[3,"Estimator"] <- c("Naive Plug-In -- Post-Lasso")
+Results[3,"ATT"] <- Compute_ATT(y,d,X,CAL$betaPL)$theta
+Results[3,"asymptoticsd"] <- Compute_ATT(y,d,X,CAL$betaPL)$sigma
 Results[3,"PropScore"] <- length(CAL$SHat)
 Results[3,"Outcome"] <- 0
 
 ### 4. Immunized Post-Lasso
-Results[4,"Estimator"] <- c("Immunized, Post-Lasso")
-Results[4,"ATT"] <- ImmunizedATT(y,d,X,CAL$betaPL,ORT_WLS_PL$muPL, Immunity=T)$theta
-Results[4,"asymptoticsd"] <- ImmunizedATT(y,d,X,CAL$betaPL,ORT_WLS_PL$muPL, Immunity=T)$sigma
+Results[4,"Estimator"] <- c("Immunized -- Post-Lasso")
+Results[4,"ATT"] <- Compute_ATT(y,d,X,CAL$betaPL,ORT_WLS_PL$muPL)$theta
+Results[4,"asymptoticsd"] <- Compute_ATT(y,d,X,CAL$betaPL,ORT_WLS_PL$muPL)$sigma
 Results[4,"PropScore"] <- length(CAL$SHat)
 Results[4,"Outcome"] <- length(ORT_WLS_PL$SHat)
 
@@ -226,35 +167,35 @@ FARRELL <- OrthogonalityReg(y,d,X,CAL$betaLasso,method="LinearOutcome",
                             maxIterPen=1e4,maxIterLasso=1e6,tolLasso=1e-6,PostLasso=T,trace=T)
 
 ### Pre-selecting the same covariates
-### NOT FAIR TO COMPARE WITH OUR PROCEDURE !!!
-FARRELL <- OrthogonalityReg(y,d,X,CAL$betaLasso,method="LinearOutcome",
+### Not faire to compare with our procedure
+FARRELL_original <- OrthogonalityReg(y,d,X,CAL$betaLasso,method="LinearOutcome",
                             c=1.1*sd(y), nopenset=c(1,3,7,8), RescaleY=T,
                             maxIterPen=1e4,maxIterLasso=1e6,tolLasso=1e-6,PostLasso=T,trace=T)
 
 
 ### Save Farrell (2015)
-Results[5,"Estimator"] <- c("Farrell (2015), Lasso")
-Results[5,"ATT"] <- ImmunizedATT(y,d,X,LOGIT$betaLasso, FARRELL$muLasso, Immunity=T)$theta
-Results[5,"asymptoticsd"] <- ImmunizedATT(y,d,X,LOGIT$betaLasso, FARRELL$muLasso, Immunity=T)$sigma
+Results[5,"Estimator"] <- c("Farrell (2015) -- Lasso")
+Results[5,"ATT"] <- Compute_ATT(y,d,X,LOGIT$betaLasso, FARRELL$muLasso)$theta
+Results[5,"asymptoticsd"] <-  Compute_ATT(y,d,X,LOGIT$betaLasso, FARRELL$muLasso)$sigma
 Results[5,"PropScore"] <- length(LOGIT$SHat)
 Results[5,"Outcome"] <- length(FARRELL$SHat)
 
-Results[6,"Estimator"] <- c("Farrell (2015), Post-Lasso")
-Results[6,"ATT"] <- ImmunizedATT(y,d,X,LOGIT$betaPL, FARRELL$muPL, Immunity=T)$theta
-Results[6,"asymptoticsd"] <- ImmunizedATT(y,d,X,LOGIT$betaPL, FARRELL$muPL, Immunity=T)$sigma
+Results[6,"Estimator"] <- c("Farrell (2015) -- Post-Lasso")
+Results[6,"ATT"] <- Compute_ATT(y,d,X,LOGIT$betaPL, FARRELL$muPL)$theta
+Results[6,"asymptoticsd"] <- Compute_ATT(y,d,X,LOGIT$betaPL, FARRELL$muPL)$sigma
 Results[6,"PropScore"] <- length(LOGIT$SHat)
 Results[6,"Outcome"] <- length(FARRELL$SHat)
 
 ### 8. Inverse propensity weighting
-Results[9,"Estimator"] <- c("IPW, Lasso")
-Results[9,"ATT"] <- Naive_ATT(y,d,X,LOGIT$betaLasso)$theta
-Results[9,"asymptoticsd"] <- Naive_ATT(y,d,X,LOGIT$betaLasso)$sigma
+Results[9,"Estimator"] <- c("IPW -- Lasso")
+Results[9,"ATT"] <- Compute_ATT(y,d,X,LOGIT$betaLasso)$theta
+Results[9,"asymptoticsd"] <- Compute_ATT(y,d,X,LOGIT$betaLasso)$sigma
 Results[9,"PropScore"] <- length(LOGIT$SHat)
 Results[9,"Outcome"] <- 0
 
-Results[10,"Estimator"] <- c("IPW, Post-Lasso")
-Results[10,"ATT"] <- Naive_ATT(y,d,X,LOGIT$betaPL)$theta
-Results[10,"asymptoticsd"] <- Naive_ATT(y,d,X,LOGIT$betaPL)$sigma
+Results[10,"Estimator"] <- c("IPW -- Post-Lasso")
+Results[10,"ATT"] <- Compute_ATT(y,d,X,LOGIT$betaPL)$theta
+Results[10,"asymptoticsd"] <- Compute_ATT(y,d,X,LOGIT$betaPL)$sigma
 Results[10,"PropScore"] <- length(LOGIT$SHat)
 Results[10,"Outcome"] <- 0
 
@@ -269,8 +210,6 @@ Results[7,"ATT"] <- BCH$theta
 Results[7,"asymptoticsd"] <- BCH$sigma
 Results[7,"PropScore"] <- length(BCH$SHatd)
 Results[7,"Outcome"] <- length(BCH$SHaty)
-
-
 
 
 ### 5. Simple linear regression
